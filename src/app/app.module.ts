d5fe9de0db4af6225f0839f71930d91f8db51ae0
Agam Singh <singh.agam0009@gmail.com>
2021-10-02 12:23:24 +1000

    simple shitty changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainTitleComponent } from './components/main-title/main-title.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainTitleComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
